a70898b8916996ad8071baba8b4fa045609e0f29
shinigamiii11 <kometmontana22@gmail.com>
2023-02-10 23:29:54 +0200

    add slider

--- src/js/script.js
+++ src/js/script.js
@@ -2,42 +2,62 @@ const sliderImages = document.querySelectorAll('.slider__img'),
       sliderDots = document.querySelectorAll('.slider__dot'),
       sliderLine = document.querySelector('.slider__line');
 
+
+    let currentSlide = 0;
+
+    const intervalTime = 5000;
+
     let sliderCount = 0,
         sliderWidth;
+    
+    window.addEventListener('resize', initSlider);
 
-    window.addEventListener('resize', showSlide);
+    function setIntervalToSlider() {
+        setInterval(() => {
+            if (sliderImages[currentSlide + 1]) {
+                currentSlide = currentSlide + 1
+                addActiveClassToDotsByIndex(currentSlide)
+                sliderCount = currentSlide
+                setTransformStyleToSlide();
+            } else {
+                currentSlide = currentSlide - 1
+                addActiveClassToDotsByIndex(currentSlide)
+                sliderCount = currentSlide
+                setTransformStyleToSlide();
+            }
+        }, intervalTime);
+    }
 
-
-    setInterval(() => {
-
-        rollSlider();
-        thisSlide(sliderCount);
-    }, 2000);
-
-
-    function showSlide() {
+    function initSlider() {
         sliderWidth = document.querySelector('.slider').offsetWidth;
         sliderLine.style.width = sliderWidth * sliderImages.length + 'px';
         sliderImages.forEach(slide => slide.style.width = sliderWidth + 'px');
-        rollSlider();
+        setTransformStyleToSlide();
+        setIntervalToSlider();
     }
-    showSlide();
 
 
-    function rollSlider() {
+    function setTransformStyleToSlide() {
         sliderLine.style.transform = `translateX(${-sliderCount * sliderWidth}px)`;
     }
 
 
-    function thisSlide(index) {
+    function addActiveClassToDotsByIndex(index) {
         sliderDots.forEach(item => item.classList.remove('active'));
         sliderDots[index].classList.add('active');
     }
 
-    sliderDots.forEach((dot, index) => {
-        dot.addEventListener('click', () => {
-            sliderCount = index;
-            rollSlider();
-            thisSlide(sliderCount);
+    function handleDotsClick() {   
+        sliderDots.forEach((dot, index) => {
+            dot.addEventListener('click', () => {
+                sliderCount = index;
+                setTransformStyleToSlide();
+                addActiveClassToDotsByIndex(index);
+            })
         })
-    })
+    }
+
+
+    initSlider();
+    handleDotsClick();
+    
